f31c073632c5d8202371dc88e5c7089c97d2ed72
ryoma-kaneko <nekowafu0413@gmail.com>
2021-05-05 23:59:01 +0900

    change!

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Image from 'next/image'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 export default function Home() {
   return (
-    <Layout title="Home!">
-      <p className="text-4xl">Hello Nextjs!</p>
+    <Layout title="Home">
+      <p className="text-4xl">Welcome to Nextjs</p>
     </Layout>
-  )
+  );
 }
